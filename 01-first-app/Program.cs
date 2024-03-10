1c9d9bbf79519676ff338cdca26c411430d96d0d
Leoozito <leonardonogueira616@gmail.com>
2024-03-09 23:39:34 -0300

    Aplicando switch case e estruturando pastas

--- 01-first-app/Program.cs
+++ 01-first-app/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ExibirMensagemDeBoasVindas() {
+    Console.WriteLine(@"
+
+    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗
+    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║
+    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║
+    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║
+    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║
+    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝
+
+    ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+    ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+    ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+    ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+    ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+    ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+
+    ");
+    Console.WriteLine("Seja bem vindo ao Screen Sound");
+}
+
+void ExibirOpcoesDoMenu() {
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica) {
+        case 1:
+            Console.WriteLine("Você escolhe a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2:
+            Console.WriteLine("Você escolhe a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("Você escolhe a opção " + opcaoEscolhidaNumerica);
+            break;    
+        case 4:
+            Console.WriteLine("Você escolhe a opção " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("Tchau tchau :)");
+            break;
+        default: 
+            Console.WriteLine("Opção invalida!");
+            break;
+    }
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
--- 01-first-app/doc-annotation.md
+++ 01-first-app/doc-annotation.md
@@ -1,2 +1,6 @@
 a documentação do .NET Framework: https://learn.microsoft.com/pt-br/dotnet/framework/
 a documentação do C#: https://learn.microsoft.com/pt-br/dotnet/csharp/
+
+Comando para criar um projeto em C#
+
+```dotnet new console```
