2064213e6911ec11d71a5bcb2703400be61a5b94
Leoozito <leonardonogueira616@gmail.com>
2024-03-10 00:48:02 -0300

    Exercitando - Feito jogo de random

--- exercising/Program.cs
+++ exercising/Program.cs
@@ -1,6 +1,32 @@
 ﻿void Apresentacao() {
-    Console.WriteLine("Acerte o número que a máquina está pensando!");
-    Console.WriteLine("");
+    // numero que a maquina definir
+    Random random = new Random();
+    int numRandomMaquina = random.Next(1, 101);
+
+    Console.WriteLine("\nAcerte o número que a máquina está pensando! (de 0 a 100)");
+
+    for (int numTentativas = 7; numTentativas > 0; numTentativas--) {
+        
+        Console.WriteLine("Você tem "+ numTentativas + " tentativas !");
+        Console.Write("\nDigite um numero: ");
+        string escolha = Console.ReadLine()!;
+        int numEscolhido = int.Parse(escolha);
+
+        if (numRandomMaquina == numEscolhido) {
+            Console.WriteLine("\nACERTOU !!");
+            Console.WriteLine("O N° que a máquina tava pensando era: [" + numRandomMaquina + "]");
+            break;
+        } else if (numRandomMaquina > numEscolhido) {
+            Console.WriteLine("\nO número da máquina é MAIOR");
+        } else if (numRandomMaquina < numEscolhido) {
+            Console.WriteLine("\nO número da máquina é MENOR");
+        } else {
+            Console.WriteLine("\nDigite um número valido (de 0 a 100)");
+            numTentativas = numTentativas;
+        }
+    };
+    Console.WriteLine("N° da máquina era: [" + numRandomMaquina + "]");
+
 }
 
 Apresentacao();
