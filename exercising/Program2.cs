82d976bac0c2965b22bca1658443591d5ff34628
Leoozito <leonardonogueira616@gmail.com>
2024-03-10 19:05:25 -0300

    Pratica com lista completa

--- exercising/Program2.cs
+++ exercising/Program2.cs
@@ -1,28 +1,48 @@
 List<int> todosNumeros = new List<int>();
 
 void CalculaNumerosEmLista() {
-    Console.WriteLine("Calculo de todos números em uma lista");
+    Console.WriteLine("\n************************************");
+    Console.WriteLine("Calculo de números em uma lista");
+    Console.WriteLine("************************************\n");
 
-    int teclaContinuacao;
+    int teclaContinuacao = 1;
 
-    while (teclaContinuacao != 0) {
+    while (teclaContinuacao == 1) {
         AdicionaNumerosNaLista();
         Console.WriteLine(@"
-            Tecle [ 0 ] para continuar adicionando números na lista
-            Tecle [ 1 ] para parar o programa
+Tecle [ 1 ] para AUMENTAR a lista !
+Tecle [ 2 ] para PARAR e VER O RESULTADO do calculo !
         ");
 
+        string tecla = Console.ReadLine()!;
+        teclaContinuacao = int.Parse(tecla);
+
+        if (teclaContinuacao == 2) {
+            RetornaCalculoSoma(todosNumeros);
+        }
     }
 }
 
-int AdicionaNumerosNaLista() {
-        Console.Write("Digite qualquer número que você queira adicionar na lista: ");
-        string numeroDigitado = Console.ReadLine()!;
-        int numeroDigitadoInt = int.Parse(numeroDigitado);
+void AdicionaNumerosNaLista() {
+    Console.Write("Digite qualquer número que você queira adicionar na lista: ");
+    string numeroDigitado = Console.ReadLine()!;
+    int numeroDigitadoInt = int.Parse(numeroDigitado);
 
-        todosNumeros.Add(numeroDigitado);
+    todosNumeros.Add(numeroDigitadoInt);
 
-        Console.WriteLine($"Lista montada: {todosNumeros}");
+    Console.WriteLine($"Lista montada: {string.Join(", ", todosNumeros)}");
+}
+
+void RetornaCalculoSoma(List<int> todosNumeros) {
+    int valorTotalNumeros = 0;
+
+    foreach (int cadaNumero in todosNumeros) {
+        valorTotalNumeros += cadaNumero;
+    }
+
+    Console.Clear();
+    Console.WriteLine($"Sua lista: {string.Join(", ", todosNumeros)}");
+    Console.WriteLine($"Soma de todos os numeros da lista: {valorTotalNumeros}");
 }
 
 CalculaNumerosEmLista();
--- my-app/Program.cs
+++ my-app/Program.cs
@@ -1,57 +1,8 @@
-﻿// Console.WriteLine(@"
+﻿Console.WriteLine(@"
 
-// █████████████████████████████████████
-// █▄─▀█▀─▄█─▄▄─█─▄─▄─█▄─▄█─▄▄─█▄─▀█▄─▄█
-// ██─█▄█─██─██─███─████─██─██─██─█▄▀─██
-// ▀▄▄▄▀▄▄▄▀▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▄▄▄▄▀▄▄▄▀▀▄▄▀
+█████████████████████████████████████
+█▄─▀█▀─▄█─▄▄─█─▄─▄─█▄─▄█─▄▄─█▄─▀█▄─▄█
+██─█▄█─██─██─███─████─██─██─██─█▄▀─██
+▀▄▄▄▀▄▄▄▀▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▄▄▄▄▀▄▄▄▀▀▄▄▀
 
-// ");
-
-List<int> todosNumeros = new List<int>();
-
-void CalculaNumerosEmLista() {
-    Console.WriteLine("\n************************************");
-    Console.WriteLine("Calculo de números em uma lista");
-    Console.WriteLine("************************************\n");
-
-    int teclaContinuacao = 1;
-
-    while (teclaContinuacao == 1) {
-        AdicionaNumerosNaLista();
-        Console.WriteLine(@"
-Tecle [ 1 ] para AUMENTAR a lista !
-Tecle [ 2 ] para PARAR e VER O RESULTADO do calculo !
-        ");
-
-        string tecla = Console.ReadLine()!;
-        teclaContinuacao = int.Parse(tecla);
-
-        if (teclaContinuacao == 2) {
-            RetornaCalculoSoma(todosNumeros);
-        }
-    }
-}
-
-void AdicionaNumerosNaLista() {
-    Console.Write("Digite qualquer número que você queira adicionar na lista: ");
-    string numeroDigitado = Console.ReadLine()!;
-    int numeroDigitadoInt = int.Parse(numeroDigitado);
-
-    todosNumeros.Add(numeroDigitadoInt);
-
-    Console.WriteLine($"Lista montada: {string.Join(", ", todosNumeros)}");
-}
-
-void RetornaCalculoSoma(List<int> todosNumeros) {
-    int valorTotalNumeros = 0;
-
-    foreach (int cadaNumero in todosNumeros) {
-        valorTotalNumeros += cadaNumero;
-    }
-
-    Console.Clear();
-    Console.WriteLine($"Sua lista: {string.Join(", ", todosNumeros)}");
-    Console.WriteLine($"Soma de todos os numeros da lista: {valorTotalNumeros}");
-}
-
-CalculaNumerosEmLista();
+");
